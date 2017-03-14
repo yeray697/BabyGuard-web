ce3703e79b213966cc97a4e3eefc09a37b944feb
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-03-14 23:03:10 +0100

    Table is passed to a function

--- functions.php
+++ functions.php
@@ -36,6 +36,112 @@ function printNav($active,$classes, $id){
             </ul>
     <?php
 }
+function loadManageIds(){
+    ?>    <thead>
+                                <tr>
+                                    <th data-field="id">Id</th>
+                                    <th data-field="name">Name</th>
+                                    <th data-field="email">Email</th>
+                                    <th data-field="options">Options</th>
+                                </tr>
+                            </thead>
+    <?php
+}
+function loadManageContent(){
+    ?><tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Alvin</td>
+                                    <td>Eclair@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Alan</td>
+                                    <td>Jellybean@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Jonathan</td>
+                                    <td>Lollipop@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>Alvin</td>
+                                    <td>Eclair@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>Alan</td>
+                                    <td>Jellybean@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>Jonathan</td>
+                                    <td>Lollipop@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>Alvin</td>
+                                    <td>Eclair@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>Alan</td>
+                                    <td>Jellybean@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>Jonathan</td>
+                                    <td>Lollipop@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>Alvin</td>
+                                    <td>Eclair@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>11</td>
+                                    <td>Alan</td>
+                                    <td>Jellybean@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>12</td>
+                                    <td>Jonathan</td>
+                                    <td>Lollipop@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>13</td>
+                                    <td>Alvin</td>
+                                    <td>Eclair@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>14</td>
+                                    <td>Alan</td>
+                                    <td>Jellybean@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                                <tr>
+                                    <td>15</td>
+                                    <td>Jonathan</td>
+                                    <td>Lollipop@gmail.com</td>
+                                    <td><i class="material-icons">mode_edit</i> <i class="material-icons">delete</i></td>
+                                </tr>
+                            </tbody>
+    <?php
+}
 function printFooter($print_footer){
 ?>
     <?php if ($print_footer) : ?>
